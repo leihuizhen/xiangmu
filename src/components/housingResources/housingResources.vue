550a9517ab57a6103704a3e652d15603a80fcadb
leihuizhen <215609495@qq.com>
2017-09-19 12:15:42 +0800

    基本结构

---
+++ src/components/housingResources/housingResources.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                message: '我是房源页面'
+            }
+        }
+    }
+</script>
