8c55fe370e957627e2a15d1db43937f3e24b5267
leihuizhen <215609495@qq.com>
2017-09-19 12:12:45 +0800

    基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
-    <div class="tmpl">
-        <!-- 头部 -->
-        <mt-header fixed title="Vue内容管理系统"></mt-header>
-        <!-- 全局返回按钮 -->
-        <div class="back" v-if="isShow">
-            <a @click="goBack">&lt;返回</a>
-        </div>
-
+    <div>
         <!-- 中间就显示路由跳转过去之后内容显示在这里 -->
-        <router-view></router-view>
+        <router-view v-wechat-title="$route.meta.title"></router-view>
 
         <!-- 底部 -->
         <nav class="mui-bar mui-bar-tab">
@@ -16,19 +9,19 @@
                 <span class="mui-icon mui-icon-home"></span>
                 <span class="mui-tab-label">首页</span>
             </router-link>
-            <router-link class="mui-tab-item" to="/member">
+            <router-link class="mui-tab-item" to="/housingResources">
                 <span class="mui-icon mui-icon-email"></span>
-                <span class="mui-tab-label">会员</span>
+                <span class="mui-tab-label">房源</span>
             </router-link>
-            <router-link class="mui-tab-item" to="/shopcart">
+            <router-link class="mui-tab-item" to="/myCollection">
                 <span class="mui-icon mui-icon-contact">
-                    <span id="badgeId" class="mui-badge">{{totalCount}}</span>
+                   <!-- <span id="badgeId" class="mui-badge">{{totalCount}}</span>-->
                 </span>
-                <span class="mui-tab-label">购物车</span>
+                <span class="mui-tab-label">收藏</span>
             </router-link>
-            <router-link class="mui-tab-item" to="/settings">
+            <router-link class="mui-tab-item" to="/mine">
                 <span class="mui-icon mui-icon-gear"></span>
-                <span class="mui-tab-label">设置</span>
+                <span class="mui-tab-label">我的</span>
             </router-link>
         </nav>
     </div>
@@ -50,23 +43,26 @@
         color: #fff;
         font-weight: bold;
     }
+ .mui-bar-tab .mui-tab-item.mui-active{
+    color: #EC5428; /* 这里放你想要的颜色 */
+}
 </style>
 
 <script>
 
     //导入公共的vue对象
-    import {vueObj} from './tools/commonvue.js';
+  //  import {vueObj} from './tools/commonvue.js';
 
-    import {getGoods} from './tools/shopCartHelper.js';
+  //  import {getGoods} from './tools/shopcartHelper.js';
 
-    import $ from 'jquery';
+    //import $ from 'jquery';
 
     //接收值
     /*
     * 1.注册广播
     * 2.接收值(回调函数)
     * */
-    vueObj.$on('shopCount',function (count) {
+   // vueObj.$on('shopCount',function (count) {
 
         //this.badge = count; 这种方式是不行的
         //原因是 : this 不代表 App.vue这个对象
@@ -74,70 +70,70 @@
 
         var oldValue = badgeObj.innerText - 0;*/
 
-        var badgeObj = $("#badgeId");
+    //    var badgeObj = $("#badgeId");
 
-        var oldValue = badgeObj.text() - 0;
+      //  var oldValue = badgeObj.text() - 0;
 
         //将传进来的数量进行累加
-        oldValue+=count;
+  //      oldValue+=count;
 
         /*badgeObj.innerText = oldValue;*/
-        badgeObj.text(oldValue);
-    })
+   //   badgeObj.text(oldValue);
+  //  })
 
 
-    export default{
+   // export default{
         /**
          * 监控路由的跳转
          * */
-        data(){
-          return {
-              totalCount:0,
-              isShow : false
-          }
-        },
-        watch:{
-            '$route':function (newValue,oldValue) {
-                if (newValue.path.toLowerCase()=='/home'||
-                        newValue.path.toLowerCase()=='/member' ||
-                        newValue.path.toLowerCase()=='/shopcart' ||
-                        newValue.path.toLowerCase()=='/settings'){
-                    this.isShow = false;
-                }else{
-                    this.isShow = true;
-                }
-            }
-        },
-        created(){
-            //给window添加load事件
-            window.addEventListener('load', this.windowOnLoad);
-            //获取购物车中商品的总数
-            this.getTotalCount();
-        },
-        methods:{
-            goBack(){
-                this.$router.go(-1);//回到上一个页面,相当于history.go(-1)
-            },
-            windowOnLoad(){
-                if (this.$route.path.toLowerCase() == '/home' ||
-                        this.$route.path.toLowerCase() == '/member' ||
-                        this.$route.path.toLowerCase() == '/shopcart' ||
-                        this.$route.path.toLowerCase() == '/settings'){
-                    this.isShow = false;
-                }else{
-                    this.isShow = true;
-                }
-            },
-            getTotalCount(){
-                var goodsArray = getGoods();
-                let totalCount = 0;
-                for(var i=0;i<goodsArray.length;i++){
-                    const goodsObj = goodsArray[i];
-                    totalCount += goodsObj.count;
-                }
-
-                this.totalCount = totalCount;
-            }
-        }
-    }
+    //    data(){
+    //      return {
+     //         totalCount:0,
+     //         isShow : false
+      //    }
+      //  },
+//    //  watch:{
+//          '$route':function (newValue,oldValue) {
+//              if (newValue.path.toLowerCase()=='/home'||
+//                      newValue.path.toLowerCase()=='/housingResources' ||
+//                      newValue.path.toLowerCase()=='/myCollection' ||
+//                      newValue.path.toLowerCase()=='/mine'){
+//                  this.isShow = false;
+//              }else{
+//                  this.isShow = true;
+//              }
+//          }
+//      },
+//      created(){
+//          //给window添加load事件
+//          window.addEventListener('load', this.windowOnLoad);
+//          //获取购物车中商品的总数
+//          this.getTotalCount();
+//      },
+//      methods:{
+//          goBack(){
+//              this.$router.go(-1);//回到上一个页面,相当于history.go(-1)
+//          },
+//          windowOnLoad(){
+//              if (this.$route.path.toLowerCase() == '/home' ||
+//                      this.$route.path.toLowerCase() == '/housingResources' ||
+//                      this.$route.path.toLowerCase() == '/shopcart' ||
+//                      this.$route.path.toLowerCase() == '/mine'){
+//                  this.isShow = false;
+//              }else{
+//                  this.isShow = true;
+//              }
+//          },
+//          getTotalCount(){
+//              var goodsArray = getGoods();
+//              let totalCount = 0;
+//              for(var i=0;i<goodsArray.length;i++){
+//                  const goodsObj = goodsArray[i];
+//                  totalCount += goodsObj.count;
+//              }
+//
+//              this.totalCount = totalCount;
+//          }
+//      }
+//  }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 /**
- * Created by huangwei on 2017/2/27.
  */
 //导入vue这个包 (使用ES6新语法)
 //var Vue = required('vue')
@@ -7,6 +6,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import VuePreview from 'vue-preview'
+
+
+
+import VueWechatTitle from 'vue-wechat-title';  
+Vue.use(VueWechatTitle)
 
 //集成图片预览的
 Vue.use(VuePreview)
@@ -36,8 +40,8 @@ import '../statics/css/site.css'
 import moment from 'moment';
 
 Vue.filter('dateFmt', (input, formatString) => {
-    formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
-    return moment(input).format(formatString);
+	formatString = formatString || 'YYYY-MM-DD HH:mm:ss';
+	return moment(input).format(formatString);
 })
 
 //使用路由
@@ -45,42 +49,42 @@ Vue.use(VueRouter)
 
 //导入组件
 import home from './components/home/home.vue'
-import member from './components/member/member.vue'
-import settings from './components/settings/settings.vue'
-import shopcart from './components/shopcart/shopcart.vue'
-import newslist from './components/news/newslist.vue'
-import newsinfo from './components/news/newsinfo.vue'
-import photolist from './components/photo/photolist.vue'
-import photoinfo from './components/photo/photoinfo.vue'
-import goodslist from './components/goods/goodslist.vue'
-import goodsinfo from './components/goods/goodsinfo.vue'
-import goodsdesc from './components/goods/goodsdesc.vue'
-import goodscomment from './components/goods/goodscomment.vue'
+import housingResources from './components/housingResources/housingResources.vue'
+import mine from './components/mine/mine.vue'
+import myCollection from './components/myCollection/myCollection.vue'
+//import newslist from './components/news/newslist.vue'
+//import newsinfo from './components/news/newsinfo.vue'
+//import photolist from './components/photo/photolist.vue'
+//import photoinfo from './components/photo/photoinfo.vue'
+//import goodslist from './components/goods/goodslist.vue'
+//import goodsinfo from './components/goods/goodsinfo.vue'
+//import goodsdesc from './components/goods/goodsdesc.vue'
+//import goodscomment from './components/goods/goodscomment.vue'
 
 //创建并且配置好路由
 const vueRouter = new VueRouter({
-    routes : [
-        {name:'/',path:'/',redirect:'home'},
-        {name:'home',path:'/home',component:home},
-        {name:'member',path:'/member',component:member},
-        {name:'settings',path:'/settings',component:settings},
-        {name:'shopcart',path:'/shopcart',component:shopcart},
-        {name:'newslist',path:'/news/list',component:newslist},
-        {name:'newsinfo',path:'/news/info/:newsId',component:newsinfo},
-        {name:'photolist',path:'/photo/list',component:photolist},
-        {name:'photoinfo',path:'/photo/info/:photoId',component:photoinfo},
-        {name:'goodslist',path:'/goods/list',component:goodslist},
-        {name:'goodsinfo',path:'/goods/info/:goodsId',component:goodsinfo},
-        {name:'goodsdesc',path:'/goods/desc/:goodsId',component:goodsdesc},
-        {name:'goodscomment',path:'/goods/comment/:goodsId',component:goodscomment}
-    ],
-    linkActiveClass : 'mui-active' //当我们路由(链接)激活的时候，给被激活的
-    //路由动态添加 mui-active这个样式
+	routes : [
+		{name:'/',path:'/',redirect:'home'},
+		{name:'home',path:'/home',component:home, meta: {title: '在线开盘'}},
+		{name:'housingResources',path:'/housingResources',component:housingResources,meta: {title: '房源列表'}},
+		{name:'myCollection',path:'/myCollection',component:myCollection,meta: {title: '我的收藏'}},
+		{name:'mine',path:'/mine',component:mine,meta: {title: '我的'}},
+//		{name:'newslist',path:'/news/list',component:newslist},
+//		{name:'newsinfo',path:'/news/info/:newsId',component:newsinfo},
+//		{name:'photolist',path:'/photo/list',component:photolist},
+//		{name:'photoinfo',path:'/photo/info/:photoId',component:photoinfo},
+//		{name:'goodslist',path:'/goods/list',component:goodslist},
+//		{name:'goodsinfo',path:'/goods/info/:goodsId',component:goodsinfo},
+//		{name:'goodsdesc',path:'/goods/desc/:goodsId',component:goodsdesc},
+//		{name:'goodscomment',path:'/goods/comment/:goodsId',component:goodscomment}
+	],
+	linkActiveClass : 'mui-active' //当我们路由(链接)激活的时候，给被激活的
+	//路由动态添加 mui-active这个样式
 }) ;
 
 //创建Vue对象
 new Vue({
    el:'#app',
-    router : vueRouter,
-    render : c=>c(App) //到时候去渲染哪一个组件作为我们App的入口文件
+	router : vueRouter,
+	render : c=>c(App) //到时候去渲染哪一个组件作为我们App的入口文件
 });
